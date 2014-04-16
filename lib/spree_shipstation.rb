86a860b9800b6af36bf38ca9d39b063e4a628cff
Perri Lapidus <plapidus@gmail.com>
2014-04-16 11:20:15 -0400

    module Spree causing error

--- lib/spree_shipstation.rb
+++ lib/spree_shipstation.rb
@@ -2,6 +2,6 @@ require 'spree_core'
 require 'spree_shipstation/engine'
 require 'spree/shipment_notice'
 
-module Spree
-  extend ActionView::Helpers:TagHelper
-end
+# module Spree
+#   extend ActionView::Helpers:TagHelper
+# end
